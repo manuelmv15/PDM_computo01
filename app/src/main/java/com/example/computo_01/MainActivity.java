ceb96a8fc06ecc296be838a881fcaae49500df59
manuelmv15 <mv23036@ues.edu.sv>
2026-03-12 10:06:00 -0600

    como un textwatcher

--- app/src/main/java/com/example/computo_01/MainActivity.java
+++ app/src/main/java/com/example/computo_01/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.computo_01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText input;
+
+    TextView output;
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        input = findViewById(R.id.editTextText);
+
+        output = findViewById(R.id.text);
+
+        btn = findViewById(R.id.button1);
+
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString().toUpperCase();
+
+
+                output.setText(text);
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+        });
+
     }
 }
